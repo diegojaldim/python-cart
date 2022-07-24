b18c28585fa5da69852558bdbbc6fb1e05587731
djaldim <diego@jettax.com.br>
2022-07-24 13:35:15 -0300

    Apply discount

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from itertools import product
 from product import Product
 
-caneta = Product('Caneta Bic', 1.99, 2)
+ps5 = Product('Playstation 5', 4999.50, 1)
+ps5.apply_discount(20)
+
+print(ps5.price)
--- product.py
+++ product.py
@@ -1,5 +1,9 @@
 class Product:
+    
     def __init__(self, name, price, qty):
         self.name = name
         self.price = price
         self.qty = qty
+
+    def apply_discount(self, percentage):
+        self.price -= (self.price * percentage) / 100
