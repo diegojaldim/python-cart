0c47d0754a27ffb4f3f52ac81bb9389a582c37a8
djaldim <diego@jettax.com.br>
2022-07-24 13:24:03 -0300

    Create product class

--- app.py
+++ app.py
@@ -1,1 +1,4 @@
-print("Hello World")
+from itertools import product
+from product import Product
+
+caneta = Product('Caneta Bic', 1.99, 2)
---
+++ product.py
@@ -0,0 +1,5 @@
+class Product:
+    def __init__(self, name, price, qty):
+        self.name = name
+        self.price = price
+        self.qty = qty
